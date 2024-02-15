b37b3f9d6196fde5f84f116e10481d3bfebe39c4
riefuchi220 <0896.riri@gmail.com>
2024-02-16 00:07:01 +0900

    PokeListコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PokeList from "./components/PokeList.vue";
+</script>
 
 <template>
   <h1>Poke API</h1>
+  <PokeList />
 </template>
 
 <style scoped></style>
